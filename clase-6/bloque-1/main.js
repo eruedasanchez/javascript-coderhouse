26c0667e2d16e1a9adcc023b6d3abad4040d569e
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-24 12:30:14 -0300

    Add class-6. Second part

--- clase-6/bloque-1/main.js
+++ clase-6/bloque-1/main.js
@@ -1,120 +1,120 @@
 /* Clase 6. Eventos. Bloque 1 */
 
-let boton = document.getElementById("boton"); // Acceso al boton
+// let boton = document.getElementById("boton"); // Acceso al boton
 
-const respuesta = () => {
-    console.log("click");
-}
+// const respuesta = () => {
+//     console.log("click");
+// }
 
-Primera forma de agregar un evento
-boton.addEventListener("click", respuesta);  // Primer parametro nombre del evento y segundo parametro, declaracion de la funcion
+// Primera forma de agregar un evento
+// boton.addEventListener("click", respuesta);  // Primer parametro nombre del evento y segundo parametro, declaracion de la funcion
 
-Segunda forma de agregar un evento
-boton.onclick = respuesta;                      // Tambien se asgina la declaracion de la funcion
+// Segunda forma de agregar un evento
+// boton.onclick = respuesta;                      // Tambien se asgina la declaracion de la funcion
 
-let evento = prompt("Ingrese el nombre del evento.")
-boton.addEventListener(evento, respuesta); // evento dblclick = doble click
+// let evento = prompt("Ingrese el nombre del evento.")
+// boton.addEventListener(evento, respuesta); // evento dblclick = doble click
 
-let boton = document.getElementById("boton"); // Acceso al boton
+// let boton = document.getElementById("boton"); // Acceso al boton
 
-const respuesta = (nombre) => {
-    alert(`Hola ${nombre}`);
-}
+// const respuesta = (nombre) => {
+//     alert(`Hola ${nombre}`);
+// }
 
-boton.addEventListener("click", () => respuesta("pepe"));           // Forma de declarar cuando tengo al menos un parametro
+// boton.addEventListener("click", () => respuesta("pepe"));           // Forma de declarar cuando tengo al menos un parametro
 
 /************ EVENTOS MAS COMUNES EN MOUSE EN JS ************/
 
-let boton = document.getElementById("boton"); 
-let saludo = document.getElementById("saludo");
+// let boton = document.getElementById("boton"); 
+// let saludo = document.getElementById("saludo");
 
-boton.addEventListener("mousedown", () => console.log("mousedown")); // mousedown ejecuta el evento al hacer click pero sin necesidad de soltar el click
+// boton.addEventListener("mousedown", () => console.log("mousedown")); // mousedown ejecuta el evento al hacer click pero sin necesidad de soltar el click
 
-boton.addEventListener("mouseup", () => console.log("mouseup")); // mouseup ejecuta el evento recien al hacer click y soltar el click
+// boton.addEventListener("mouseup", () => console.log("mouseup")); // mouseup ejecuta el evento recien al hacer click y soltar el click
 
-boton.addEventListener("mouseover", () => console.log("mouseover")); // mouseover ejecuta el evento al pararse sobre el boton
+// boton.addEventListener("mouseover", () => console.log("mouseover")); // mouseover ejecuta el evento al pararse sobre el boton
 
-boton.addEventListener("mouseout", () => console.log("mouseout")); // mouseout ejecuta el evento al pararse sobre el boton y luego salir
+// boton.addEventListener("mouseout", () => console.log("mouseout")); // mouseout ejecuta el evento al pararse sobre el boton y luego salir
 
-boton.addEventListener("mousemove", () => console.log("mousemove")); // mousemove ejecuta el evento unicamente al pararse sobre el boton
+// boton.addEventListener("mousemove", () => console.log("mousemove")); // mousemove ejecuta el evento unicamente al pararse sobre el boton
 
-boton.addEventListener("click", () => console.log("click")); // click ejecuta el evento unicamente al clickear sobre el boton
+// boton.addEventListener("click", () => console.log("click")); // click ejecuta el evento unicamente al clickear sobre el boton
 
-boton.addEventListener("dblclick", () => console.log("dblclick")); // dblclick ejecuta el evento unicamente al realizar doble click sobre el boton
+// boton.addEventListener("dblclick", () => console.log("dblclick")); // dblclick ejecuta el evento unicamente al realizar doble click sobre el boton
 
-boton.addEventListener("mousedown", () => {
-saludo.className = "amarillo"
-}); 
-boton.addEventListener("mouseup", () => {
-saludo.className = "azul"
-}); 
-boton.addEventListener("mouseover", () => {
-saludo.className = "rojo"
-});
-boton.addEventListener("mouseout", () => {
-saludo.className = "verde"    
-});
+// boton.addEventListener("mousedown", () => {
+// saludo.className = "amarillo"
+// }); 
+// boton.addEventListener("mouseup", () => {
+// saludo.className = "azul"
+// }); 
+// boton.addEventListener("mouseover", () => {
+// saludo.className = "rojo"
+// });
+// boton.addEventListener("mouseout", () => {
+// saludo.className = "verde"    
+// });
 
 /************ EVENTOS MAS COMUNES EN TECLADO EN JS ************/
 
-let saludo = document.getElementById("saludo");
-let nombre = document.getElementById("nombre");
+// let saludo = document.getElementById("saludo");
+// let nombre = document.getElementById("nombre");
 
-nombre.addEventListener("keydown", () => console.log("keydown")); // contador de teclas que fueron presionadas
-nombre.addEventListener("keyup", () => console.log("keyup")); // contador de teclas que fueron presionadas y soltadas. Si solo fueron presionadas, no se cuentan hasta que sean soltadas 
+// nombre.addEventListener("keydown", () => console.log("keydown")); // contador de teclas que fueron presionadas
+// nombre.addEventListener("keyup", () => console.log("keyup")); // contador de teclas que fueron presionadas y soltadas. Si solo fueron presionadas, no se cuentan hasta que sean soltadas 
 
-nombre.addEventListener("keyup", (e) => {
-    console.log(e.key);  // e brinda informacion sobre la tecla que presiono el usuario y el metodo .key permite saber la tecla que fue presionada por el usuario
-});
+// nombre.addEventListener("keyup", (e) => {
+//     console.log(e.key);  // e brinda informacion sobre la tecla que presiono el usuario y el metodo .key permite saber la tecla que fue presionada por el usuario
+// });
 
-nombre.addEventListener("keyup", (e) => {
-    if(e.key == "a"){
-        saludo.className = "amarillo";
-    } else if(e.key == "s"){
-        saludo.className = "azul";
-    } else if(e.key == "d" ){
-        saludo.className = "rojo";
-    } else {
-        saludo.className = "verde";
-    }
-});
+// nombre.addEventListener("keyup", (e) => {
+//     if(e.key == "a"){
+//         saludo.className = "amarillo";
+//     } else if(e.key == "s"){
+//         saludo.className = "azul";
+//     } else if(e.key == "d" ){
+//         saludo.className = "rojo";
+//     } else {
+//         saludo.className = "verde";
+//     }
+// });
 
-nombre.addEventListener("change", () => console.log("change")); // Al clikear fuera del input, se produce un change
+// nombre.addEventListener("change", () => console.log("change")); // Al clikear fuera del input, se produce un change
 
-nombre.addEventListener("input", () => {
-    console.log(nombre.value); // reconocimiento de cada caracter que se pulsa
-});
+// nombre.addEventListener("input", () => {
+//     console.log(nombre.value); // reconocimiento de cada caracter que se pulsa
+// });
 
-nombre.addEventListener("input", () => {
-    if(!nombre.value.includes("@")){
-        saludo.className = "rojo";
-    } else {
-        saludo.className = "verde";
-    }
-});
+// nombre.addEventListener("input", () => {
+//     if(!nombre.value.includes("@")){
+//         saludo.className = "rojo";
+//     } else {
+//         saludo.className = "verde";
+//     }
+// });
 
 /************ CREACION DE FORMULARIOS ************/
 
 let formulario = document.getElementById("formulario");
 
-formulario.addEventListener("submit", () => {
-    console.log("Hola a todos");                // Se borra todo porque el comportamiento por defecto de los formularios es restaurarse 
-});
+// formulario.addEventListener("submit", () => {
+//     console.log("Hola a todos");                // Se borra todo porque el comportamiento por defecto de los formularios es restaurarse 
+// });
 
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();            // preventDefault() evita que se borra toda la informacion del formulario (que se restaure)
-    console.log("Hola a todos");
+// formulario.addEventListener("submit", (e) => {
+//     e.preventDefault();            // preventDefault() evita que se borra toda la informacion del formulario (que se restaure)
+    // console.log("Hola a todos");
     let inputs = e.target.children; // muestra la cantidad de hijos que tiene el diccionario
-    console.log(inputs);
-    console.log(inputs[0].value);
+    // console.log(inputs);
+    // console.log(inputs[0].value);
     
-    if(!inputs[0].value.includes("@")){
-        inputs[0].value = "";
-        let div = document.createElement("div");
-        div.innerHTML = "No esta el @";
-        document.body.append(div);  // Agrega al html un mensaje avisando que no se escribio el arroba
-    }
-});
+    // if(!inputs[0].value.includes("@")){
+    //     inputs[0].value = "";
+    //     let div = document.createElement("div");
+    //     div.innerHTML = "No esta el @";
+    //     document.body.append(div);  // Agrega al html un mensaje avisando que no se escribio el arroba
+    // }
+// });
 
 
 /*************** A medida que se van agregando los datos, se colocan en un div y se muestran ***************/
@@ -131,5 +131,3 @@ formulario.addEventListener("submit", (e) => {
     `;
     info.append(div);
 });
-
-
---
+++ clase-6/bloque-2/main.js
@@ -0,0 +1,143 @@
+/* Clase 6. Storage & JSON. Bloque 2 */
+
+// Storage: Espacio que hay adentro de mi navegador que nos permite almacenar informacion 
+
+/************************** LOCAL STORAGE **************************/
+
+// console.log(localStorage);
+
+// localStorage.setItem("nombre", "mariana");      // guarda la clave con su respectivo valor
+// localStorage.setItem("numero", "1");            // todo en localStorage se guarda como un string. Por eso, lo almaceno como string
+// localStorage.setItem("valor", true);
+
+// let nombre = localStorage.getItem("nombre");    // obtener el valor de la clave nombre
+// let numero = localStorage.getItem("numero");
+// let valor = localStorage.getItem("valor");
+
+// console.log(nombre);
+// console.log(numero);
+// console.log(valor);
+
+/************************** SESSION STORAGE **************************/
+
+// A diferencia del LOCAL STORAGE, se pierde toda la informacion una vez 
+// que se cierra la pagina
+
+// console.log(sessionStorage);
+
+// sessionStorage.setItem("nombre", "mariana");   
+// sessionStorage.setItem("numero", "1");
+// sessionStorage.setItem("valor", "true");
+
+// let nombre = sessionStorage.getItem("nombre");    // obtener el valor de la clave nombre
+// let numero = sessionStorage.getItem("numero");
+// let valor = sessionStorage.getItem("valor");
+
+// console.log(nombre);
+// console.log(numero);
+// console.log(valor);
+
+
+/** Recorrido del local storage **/
+
+// for(let i=0; i < localStorage.length; i++){
+//     let clave = localStorage.key(i);            // obtencion de la clave. No se listan en el orden que fueron declaradas porque key tiene asociado un id (no es un arreglo)
+//     console.log("clave", clave);
+//     console.log("valor", localStorage.getItem(clave));
+// }
+
+/** Eliminar el LOCAL/SESSION STORAGE **/
+
+// localStorage.removeItem("nombre");  // eliminacion por clave
+// localStorage.clear();               // elimina todo el local/session storage
+
+/****************************** JSON ******************************/
+
+// Json permite guardar arreglos, objetos 
+
+// let objeto = {
+//     nombre: "andres",
+//     edad: 26,
+// };
+
+// // localStorage.setItem("objeto", objeto); // muestra ['object Object'] (no sirve para objetos)
+
+// // let arreglo = [1,2,3,4,5];
+// // localStorage.setItem("objeto", arreglo); // tampoco funciona en arreglos
+
+// localStorage.setItem("objeto", JSON.stringify(objeto)); // almacena efectivamente al objeto
+// localStorage.setItem("arreglo", JSON.stringify(arreglo));
+
+// let arreglo = localStorage.getItem("arreglo");
+// let objeto = localStorage.getItem("objeto");
+
+// console.log(JSON.parse(arreglo)); // muestra efectivamente al arreglo 
+// console.log(JSON.parse(objeto));  // muestra efectivamente al objeto
+
+// const productos = [
+//     {id: 1, nombre: "camisa", precio: 1000},
+//     {id: 2, nombre: "zapato", precio: 1200},
+//     {id: 3, nombre: "media", precio: 800},
+//     {id: 4, nombre: "gorra", precio: 1500},
+// ];
+
+// const guardar = (clave,valor) => {
+//     localStorage.setItem(clave, JSON.stringify(valor));
+// };
+
+// productos.forEach(item =>{
+//     guardar(item.id, item);
+// })
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+// let usuario;
+// let usuarioStorage = sessionStorage.getItem("usuario");
+
+// // console.log(usuarioStorage); // imprime null
+
+// if(usuarioStorage){
+//     usuario = usuarioStorage;
+//     alert(`Bienvenido ${usuario} nuevamente`);
+// } else {
+//     usuario = prompt("Ingrese el usuario");
+//     alert(`Bienvenido ${usuario} es tu primera vez`);
+//     sessionStorage.setItem("usuario", usuario);
+// }
+
+// Usando localStorage una vez que se almacena el usuario, siempre se mantiene 
+// Usando sessionStorage se almacena el usuario y se mantiene hasta que se cierre la pagina
+
+const productos = [
+    {id: 1, nombre: "camisa", precio: 1000},
+    {id: 2, nombre: "zapato", precio: 1200},
+    {id: 3, nombre: "media", precio: 800},
+    {id: 4, nombre: "gorra", precio: 1500},
+];
+
+localStorage.setItem("carrito", JSON.stringify(productos));
+
+let carrito = [];
+let carritoStorage = localStorage.getItem("carrito");
+
+if(carritoStorage){
+    carrito = JSON.parse(carritoStorage);
+} else {
+    let div = document.createElement("div");
+    div.innerHTML = "El carrito esta vacio";
+    document.body.append(div);
+}
+
+carrito.forEach((item) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <h2>ID: ${item.id}</h2>
+    <p>Nombre: ${item.nombre}</p>
+    <b>${item.precio} </b>
+    </hr />
+    `;
+    document.body.append(div);
+});
+
+
+
