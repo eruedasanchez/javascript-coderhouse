fc098099b3b4c886b7850e1fe1b532ce061e90e1
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-06 22:22:58 -0300

    Add class-8. Second part

--- clase-8/bloque-2/main.js
+++ clase-8/bloque-2/main.js
@@ -102,3 +102,71 @@ const interval = setInterval(() => {
 // Counter 5
 // Se removio el intervalo
 
+/************************** PROMESAS **********************/
+
+/* Estructura de la promesa */
+
+new Promise((resolve, reject) => {
+    // codigo de la promesa
+})
+
+const futuro = (valor) => {
+    return new Promise((resolve,reject) => {
+        valor ? resolve("promesa resuelta") : reject("promesa rechazada");
+    });
+};
+
+console.log(futuro(true)); // promise { 'promesa resuelta' }
+console.log(futuro(false)); // promise { 'promesa rechazada' } pero tira error porque no capturo el error
+
+/* Lo arreglamos de la siguiente manera */
+
+futuro(false)
+    .then((response) => console.log(response))          // futuro.then  captura la respuesta en caso de que sea exitosa
+    .catch((error) => console.log(error))               // futuro.catch captura la respuesta en caso de que no sea exitosa (respuesta erronea)
+    .finally(() => console.log("proceso terminado"));   // se ejecuta siempre si la respuesta es exitosa o no. Es independiente 
+
+
+//////////////////////////////////////////////////////////
+// Base de datos (lado servidor)
+
+const DB = [
+    {id:1, nombre: "camisa", precio: 1000},
+    {id:2, nombre: "zapato", precio: 1000},
+    {id:3, nombre: "gorra", precio: 1000},
+    {id:4, nombre: "media", precio: 1000},
+];
+
+const traerProductos = () => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(DB);
+        },3000);
+    });
+};
+//////////////////////////////////////////////////////////
+
+traerProductos()
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => console.log(error));
+
+// Luego de 3seg, se cargan los productos
+
+traerProductos()
+    .then((response) => {
+        response.forEach((producto) => {
+            let div = document.createElement("div");
+            div.innerHTML = `
+            <h2>Id: ${producto.id}</h2>
+            <p>Nombre: ${producto.nombre}</p>
+            <b>Id: ${producto.precio}</b>
+            </hr>`;
+
+            document.body.append(div);
+        });
+    })
+    .catch((error) => console.log(error));
+
+// Luego de 3seg, se cargan y se genera el codigo html puesto en el innerHTML
